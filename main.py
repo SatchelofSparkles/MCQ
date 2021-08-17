5c6928a2c74a2b776524683dfd4be9c72c8d5733
SatchelofSparkles <41989412+SatchelofSparkles@users.noreply.github.com>
2021-08-17 17:19:16 +0100

    Working version - runs GUI, produces questions (based on those in data.py), updates question, answers and score after each question attempt. Crashes 5 seconds after 10th question answered (currently set to produce 10 answers) as doesn't have a check on number of questions and a finishing point.

--- main.py
+++ main.py
@@ -17,27 +17,75 @@ def produce_question_set(quantity):
 
     while len(available_questions) > quantity:
         available_questions.pop(quantity)
-    print(available_questions)
-    with open("question_set.json", "w", encoding="utf-8") as question_set:
-        question_set.write("")
-    for q in range(0, quantity):
+    question_number_1 = available_questions[0]
+    q_1 = question_data[question_number_1]["question"]
+    colours = ["green", "blue", "yellow", "red"]
+    random.shuffle(colours)
+    correct = colours[0]
+    answers = [question_data[question_number_1]["correct_answer"], question_data[question_number_1]["incorrect_answers"][0],
+               question_data[question_number_1]["incorrect_answers"][1], question_data[question_number_1]["incorrect_answers"][2]]
+    green_response = answers[colours.index("green")]
+    blue_response = answers[colours.index("blue")]
+    yellow_response = answers[colours.index("yellow")]
+    red_response = answers[colours.index("red")]
+    new_data = {"Q1":
+        {
+            "question": q_1,
+            "correct": correct,
+            "green": green_response,
+            "blue": blue_response,
+            "yellow": yellow_response,
+            "red": red_response
+        }}
+    with open("question_set.json", "w") as question_set:
+        json.dump(new_data, question_set, indent=4)
+
+    for q in range(1, quantity):
         question_number = available_questions[q]
+        numbering = q + 1
+        tag = f"Q{numbering}"
         question = question_data[question_number]["question"]
         colours = ["green", "blue", "yellow", "red"]
         random.shuffle(colours)
         correct = colours[0]
-        answers = [question_data[q]["correct_answer"], question_data[q]["incorrect_answers"][0], question_data[q]["incorrect_answers"][1], question_data[q]["incorrect_answers"][2]]
-        with open("question_set.json", "a", encoding="utf-8") as question_set:
-            num = str(q+1)
-            next_question = {
-                "question": question,
-                "correct": correct,
-                colours[0]: answers[0],
-                colours[1]: answers[1],
-                colours[2]: answers[2],
-                colours[3]: answers[3],
-            }
-            json.dump(next_question, question_set, ensure_ascii=False, indent=4)
+        answers = [question_data[question_number]["correct_answer"], question_data[question_number]["incorrect_answers"][0],
+                   question_data[question_number]["incorrect_answers"][1], question_data[question_number]["incorrect_answers"][2]]
+        with open("question_set.json", "r") as data_file:
+            next_question = {tag:
+                {
+                    "question": question,
+                    "correct": correct,
+                    colours[0]: answers[0],
+                    colours[1]: answers[1],
+                    colours[2]: answers[2],
+                    colours[3]: answers[3],
+                }}
+            data_current = json.load(data_file)
+            data_current.update(next_question)
+        with open("question_set.json", "w") as data_file:
+            json.dump(data_current, data_file, indent=4)
+
+
+    #         json.dump(next_question, question_set, ensure_ascii=False, indent=4)
+    #         question_set.write(",")
+    # question_number = available_questions[quantity - 1]
+    # question = question_data[question_number]["question"]
+    # colours = ["green", "blue", "yellow", "red"]
+    # random.shuffle(colours)
+    # correct = colours[0]
+    # answers = [question_data[quantity - 1]["correct_answer"], question_data[quantity - 1]["incorrect_answers"][0],
+    #            question_data[quantity - 1]["incorrect_answers"][1], question_data[quantity - 1]["incorrect_answers"][2]]
+    # with open("question_set.json", "a", encoding="utf-8") as question_set:
+    #     next_question = {
+    #         "question": question,
+    #         "correct": correct,
+    #         colours[0]: answers[0],
+    #         colours[1]: answers[1],
+    #         colours[2]: answers[2],
+    #         colours[3]: answers[3],
+    #     }
+    #     json.dump(next_question, question_set, ensure_ascii=False, indent=4)
+    #     question_set.write("]")
 
 
 produce_question_set(10)
--- ui.py
+++ ui.py
@@ -1,18 +1,10 @@
 from tkinter import *
 from quiz_brain import QuizBrain
-from data import question_data
 import random
+import json
 
 THEME_COLOR = "#375362"
 
-question_current = {
-            "question": "question",
-            "correct": "initial",
-            "green": "green_val",
-            "blue": "blue_val",
-            "yellow": "yellow_val",
-            "red": "red_val",
-        }
 
 # json serialiser and deserialiser, json libs
 
@@ -25,91 +17,126 @@ class QuizInterface:
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.frame = Frame()
 
-        self.score = Label(text=f"", fg="white", bg=THEME_COLOR)
+        self.value = 1
+
+        self.current_score = 0
+
+        self.score = Label(text=f"Score: {self.current_score}", fg="white", bg=THEME_COLOR)
         self.score.grid(column=1, row=0)
         self.correct_answer = "something"
 
+        with open("question_set.json", "r") as questions:
+            self.questions_for_use = json.load(questions)
+
+        self.number_of_questions = list(self.questions_for_use.keys())
+        self.final_key = str(self.number_of_questions[-1]).split("Q")
+        self.final_question = int(self.final_key[1])
+
+
         self.canvas = Canvas(width=500, height=300, bg="white")
+
+        self.question_num = f"Q{self.value}"
+
         self.question_text = self.canvas.create_text(
             250,
             150,
             width=480,
-            text="Question",
+            text="_",
             font=("Arial", 18, "italic"))
+        self.canvas.itemconfig(self.question_text, text=self.questions_for_use[self.question_num]["question"])
         self.canvas.grid(row=1, column=0, columnspan=2, pady=25)
 
-
+        # self.question_text = self.canvas.create_text(
+        #     250,
+        #     150,
+        #     width=480,
+        #     text=self.questions_for_use[self.question_num]["question"],
+        #     font=("Arial", 18, "italic"))
 
         def choose_green():
             give_feedback("green")
-            print("green")
 
         def choose_blue():
             give_feedback("blue")
-            print("blue")
 
         def choose_yellow():
             give_feedback("yellow")
-            print("yellow")
 
         def choose_red():
             give_feedback("red")
-            print("red")
 
         def give_feedback(color):
-            if color == self.correct_answer:
-                self.canvas.itemconfig(self.question_text, text=f"You chose {color}. That's right, that is!")
-                self.window.after(5000, self.get_next_question)
-            else:
-                self.canvas.itemconfig(self.question_text, text=f'You chose {color}. That was wrong. The correct answer was in the {question_current["correct"]} box')
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.question_text, text=f"")
+            with open("question_set.json", "r") as questions:
+                answers = json.load(questions)
+                n = f"Q{self.value}"
+                if color == answers[n]["correct"]:
+                    self.current_score += 1
+                    self.canvas.itemconfig(self.question_text, text=f"You chose {color}. That's right, that is!")
+                    self.Scorecard = f"Score: {self.current_score} / {self.value}"
+                    self.score.config(text=self.Scorecard)
+                else:
+                    self.canvas.itemconfig(self.question_text,
+                                           text=f'You chose {color}. That was wrong. The correct answer was in the {self.questions_for_use[n]["correct"]} box')
+                    self.Scorecard = f"Score: {self.current_score} / {self.value}"
+                    self.score.config(text=self.Scorecard)
                 self.window.after(5000, self.get_next_question)
 
-        self.button_a = Button(width=20, height=8, text="words", bg="pale green", command=choose_green)
-        self.button_a.config(font=("Arial", 14, "italic"), wraplength = 200)
+        self.button_a = Button(width=20, height=8, text=self.questions_for_use[self.question_num]["green"], bg="pale green", command=choose_green)
+        self.button_a.config(font=("Arial", 14, "italic"), wraplength=200)
         self.button_a.grid(column=0, row=2, padx=5, pady=10)
 
-        self.button_b = Button(width=20, height=8, text = "words", bg="light blue", command=choose_blue)
-        self.button_b.config(font=("Arial", 14, "italic"), wraplength = 200)
+        self.button_b = Button(width=20, height=8, text=self.questions_for_use[self.question_num]["blue"], bg="light blue", command=choose_blue)
+        self.button_b.config(font=("Arial", 14, "italic"), wraplength=200)
         self.button_b.grid(column=1, row=2, padx=5, pady=10)
 
-        self.button_c = Button(width=20, height=8, text = "words", bg="light goldenrod", command=choose_yellow)
-        self.button_c.config(font=("Arial", 14, "italic"), wraplength = 200)
+        self.button_c = Button(width=20, height=8, text=self.questions_for_use[self.question_num]["yellow"], bg="light goldenrod", command=choose_yellow)
+        self.button_c.config(font=("Arial", 14, "italic"), wraplength=200)
         self.button_c.grid(column=0, row=3, padx=5, pady=10)
 
-        self.button_d = Button(width=20, height=8, text = "words", bg="light coral", command=choose_red)
-        self.button_d.config(font=("Arial", 14, "italic"), wraplength = 200)
+        self.button_d = Button(width=20, height=8, text=self.questions_for_use[self.question_num]["red"], bg="light coral", command=choose_red)
+        self.button_d.config(font=("Arial", 14, "italic"), wraplength=200)
         self.button_d.grid(column=1, row=3, padx=5, pady=10)
 
-        self.get_next_question()
+
+        # self.question_text = self.canvas.create_text(
+        #     250,
+        #     150,
+        #     width=480,
+        #     text=self.questions_for_use[self.question_num]["question"],
+        #     font=("Arial", 18, "italic"))
 
         self.window.mainloop()
 
     def get_next_question(self):
-        self.canvas.config(bg="white")
-        self.score.config(text=f"Score: {self.quiz.score}")
-        value = random.randint(1, 25)
-        question = question_data[value]["question"]
-        colours = ["green", "blue", "yellow", "red"]
-        random.shuffle(colours)
-        correct = colours[0]
-        answers = [question_data[value]["correct_answer"], question_data[value]["incorrect_answers"][0],
-                   question_data[value]["incorrect_answers"][1], question_data[value]["incorrect_answers"][2]]
-        question_current["question"] = question
-        question_current["correct"] = correct
+        self.value = self.value + 1
+        if self.value <= self.final_question:
+            n = f"Q{self.value}"
+            self.score.config(bg=THEME_COLOR)
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.question_text, text=self.questions_for_use[n]["question"])
+            self.button_a.config(bg="pale green")
+            self.button_a.config(text=self.questions_for_use[n]["green"])
+            self.button_b.config(bg="light blue")
+            self.button_b.config(text=self.questions_for_use[n]["blue"])
+            self.button_c.config(bg="light goldenrod")
+            self.button_c.config(text=self.questions_for_use[n]["yellow"])
+            self.button_d.config(bg="light coral")
+            self.button_d.config(text=self.questions_for_use[n]["red"])
+        else:
+            self.canvas.itemconfig(self.question_text, text=[f"Quiztime over, good game!"])
 
-        for j in range(4):
-            question_current[colours[j]] = answers[j]
-        self.canvas.itemconfig(self.question_text, text = question_current["question"])
-        self.button_a.config(text = question_current["green"])
-        self.button_b.config(text = question_current["blue"])
-        self.button_c.config(text = question_current["yellow"])
-        self.button_d.config(text = question_current["red"])
+#        self.canvas.itemconfig(self.question_text, text=question_data[self.value]["question"])
+#        self.button_a.config(text = question_data[self.value]["green"])
+#        self.button_b.config(text = question_data[self.value]["blue"])
+#        self.button_c.config(text = question_data[self.value]["yellow"])
+#        self.button_d.config(text = question_data[self.value]["red"])
 
 
-
-        # else:
-        #     self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
-        #     self.button_a.config(state="disabled")
-        #     self.button_b.config(state="disabled")
-        #     self.button_c.config(state="disabled")
-        #     self.button_d.config(state="disabled")
+# else:
+#     self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+#     self.button_a.config(state="disabled")
+#     self.button_b.config(state="disabled")
+#     self.button_c.config(state="disabled")
+#     self.button_d.config(state="disabled")
